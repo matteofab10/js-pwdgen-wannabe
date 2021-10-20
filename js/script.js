339d93f573deb6d2e0d156b2e39f5944017aecca
matteofab10 <matteofabio10@gmail.com>
2021-10-20 14:56:43 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const firstName = prompt ("Inserisci il tuo nome");
+const lastName = prompt ("Inserisci il tuo cognome");
+const favoriteColor = prompt ("Inserisci il tuo colore preferito");
+
+const password = firstName + lastName + favoriteColor;
+
+console.log(password);
