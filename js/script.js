fe38e48d80f6ca62ff9373794ebef2668fabb000
matteofab10 <matteofabio10@gmail.com>
2021-10-20 17:56:33 +0200

    richiesta dati

--- js/script.js
+++ js/script.js
@@ -2,7 +2,14 @@ const firstName = prompt("Inserisci il tuo nome");
 const lastName = prompt("Inserisci il tuo cognome");
 const favoriteColor = prompt("Inserisci il tuo colore preferito");
 
-
 console.log(firstName);
 console.log(lastName);
 console.log(favoriteColor);
+
+const textToShare = `
+  Nome: ${firstName}
+  Cognome: ${lastName}
+  Colore preferito: ${favoriteColor}
+`;
+
+document.getElementById('utente').innerHTML = textToShare;
